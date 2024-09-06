ce499bfb892e4e961f4c398ef2800a765ad08663
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2024-09-06 13:28:30 +0000

    error log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   
   root 'home#index'
-  resources :perssons
   get 'home/about'
+  
+  resources :perssons
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_06_081804) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_06_121907) do
   create_table "perssons", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -25,5 +25,17 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_06_081804) do
     t.string "other_data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
