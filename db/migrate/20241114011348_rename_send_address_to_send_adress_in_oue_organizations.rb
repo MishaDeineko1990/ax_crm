55d3157b900aebc54639921844c0f78902f77568
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2024-11-18 14:47:41 +0000

    misgrate_raneme_send_adress

--- db/migrate/20241114011348_rename_send_address_to_send_adress_in_oue_organizations.rb
+++ db/migrate/20241114011348_rename_send_address_to_send_adress_in_oue_organizations.rb
@@ -1,5 +1,7 @@
 class RenameSendAddressToSendAdressInOueOrganizations < ActiveRecord::Migration[7.2]
   def change
-    rename_column :oue_organizations, :send_adress, :send_adress
+    # if column_exists?(:oue_organizations, :send_address)
+    #   rename_column :oue_organizations, :send_address, :send_adress
+    # end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_16_125911) do
     t.string "type_rent"
     t.string "factic_address"
     t.string "legal_address"
-    t.string "send_adress"
+    t.string "send_address"
     t.string "number_count_bank"
     t.string "mfo"
     t.string "name_and_requisites_bank"
